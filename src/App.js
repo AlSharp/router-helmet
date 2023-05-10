bebd47f626132cf91f36d4f66b89efe04697baac
AlSharp <elistaphilly@gmail.com>
2023-05-10 07:25:30 -0400

    edit links

--- src/App.js
+++ src/App.js
@@ -40,16 +40,16 @@ function Layout() {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/router-helmet">Home</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/router-helmet/about">About</Link>
           </li>
           <li>
-            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/router-helmet/dashboard">Dashboard</Link>
           </li>
           <li>
-            <Link to="/nothing-here">Nothing Here</Link>
+            <Link to="/router-helmet/nothing-here">Nothing Here</Link>
           </li>
         </ul>
       </nav>
@@ -132,7 +132,7 @@ function NoMatch() {
     <div>
       <h2>Nothing to see here!</h2>
       <p>
-        <Link to="/">Go to the home page</Link>
+        <Link to="/router-helmet">Go to the home page</Link>
       </p>
     </div>
   );
